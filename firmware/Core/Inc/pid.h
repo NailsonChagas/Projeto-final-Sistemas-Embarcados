92c708b7d7c6c4e70d25937bc29d6398702fd62a
Nailson Chagas <chagasnailson@gmail.com>
2025-12-01 16:26:08 -0300

    feat: estrutura basica do pid

---
+++ firmware/Core/Inc/pid.h
@@ -0,0 +1,20 @@
+#ifndef PID_H
+#define PID_H
+
+#include <stdint.h>
+
+typedef struct {
+	uint16_t duty_cycle_cvt;
+
+    float kp;
+    float ki;
+    float kd;
+
+    float e_prev;   // e(k-1)
+    float ui_prev;  // uI(k-1)
+} PID_Controller;
+
+void pid_init(PID_Controller *pid, float kp, float ki, float kd, uint16_t time_counter, uint8_t max_input);
+uint16_t pid_compute(PID_Controller *pid, float reference, float measurement);
+
+#endif
---
+++ firmware/Core/Src/pid.c
@@ -0,0 +1,33 @@
+#include "pid.h"
+
+void pid_init(PID_Controller *pid, float kp, float ki, float kd, uint16_t time_counter, uint8_t max_input)
+{
+	/*
+	 * Controlador PID
+	 * uP(k) = Kp*e(k)
+	 * uI(k) = uI(k-1) + Ki*e(k)
+	 * uD(k) = Kd*(e(k) - e(k-1))
+	 * uPID = uP(k) + uI(k) + uD(k)
+	 **/
+	pid->duty_cycle_cvt = time_counter/max_input;
+
+    pid->kp = kp;
+    pid->ki = ki;
+    pid->kd = kd;
+
+    pid->e_prev = 0.0f;
+    pid->ui_prev = 0.0f;
+}
+
+uint16_t pid_compute(PID_Controller *pid, float reference, float measurement)
+{
+    float e = reference - measurement;
+    float ui = pid->ui_prev + pid->ki * e;
+
+    float pid_output = (pid->kp * e) + ui + (pid->kd * (e - pid->e_prev));
+
+    pid->e_prev = e;
+    pid->ui_prev = ui;
+
+    return (uint16_t)(pid_output * pid->duty_cycle_cvt);
+}
